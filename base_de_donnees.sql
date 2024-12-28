ffa84b4384edcf5f0e94328f5a2c534662f4ce1e
nourane-hammouda <160539719+nourane-hammouda@users.noreply.github.com>
2024-12-28 19:27:25 +0100

    Update base_de_donnees.sql

--- base_de_donnees.sql
+++ base_de_donnees.sql
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS Utilisateurs (
 CREATE TABLE IF NOT EXISTS Preferences_utilisateur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     utilisateur_id INT NOT NULL,
-    niveau_cuisine ENUM('débutant', 'intermédiaire', 'avancé') NOT NULL,
-    temps_preparation ENUM('15 min', '30 min', '1 h') NOT NULL,
-    ingredients TEXT,
+    niveau_cuisine ENUM('Débutant', 'Pas mal', 'Je suis un chef') NOT NULL,
+    temps_preparation ENUM('15 min', '30 min', '1 h','j'ai tout mon temps') NOT NULL,
+    nombre_de_personne INT NOT NULL,
     date_mise_a_jour DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id) ON DELETE CASCADE
 );
